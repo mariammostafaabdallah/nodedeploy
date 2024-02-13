caaa42a1e1eb31f001b76dca1c91f47e60cf38dd
mariam <mariammostafa72000@gmail.com>
2024-02-13 04:22:03 +0200

    second

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ const productRoute =require("./routes/productRoute");
 const contentRoute =require("./routes/contentRoute");
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/Shop').then(function(data)
-{
-console.log("connected")
-}).catch(err=>{
-    console.log(err)
-});
+
+mongoose.connect('mongodb://127.0.0.1:27017/Shop', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // Increase timeout value (example: 30 seconds)
+  }).then(function(data){
+    console.log("connected")
+    }).catch(err=>{
+        console.log(err)
+    });
+    
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
